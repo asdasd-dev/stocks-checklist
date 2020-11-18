ac1945c2dc301b652abd5c085289e256f5ddf8cc
asdasd-dev <vshvc05@gmail.com>
2020-11-18 19:49:23 +0300

    Add router basename for github pages

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,11 @@ import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, HashRouter } from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename='/stocks-checklist'>
       <Provider store={store}>
         <App />
       </Provider>
